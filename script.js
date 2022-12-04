3f8fdfb8ccb722738ae3fbbf66ef9bb4cda94de1
Igor Abramov <igor14abramov@gmail.com>
2022-12-04 19:11:42 +0300

    Отработал и решил первую задачу на джс.

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-console.log('Good day.')
-
+let work = 9*5;
+console.log('can i complete this? ' + (work>40));
+let money = 80;
+let zarik = money * 40;
+console.log('This will be cost : ' + zarik);
